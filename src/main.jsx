25854c32efc52f155dfc8ec75653b3ffb3ca7a40
shahbozshirinboyev <shahboz.sh.b@gmail.com>
2024-09-26 16:18:16 +0500

    delete Task component

--- src/main.jsx
+++ src/main.jsx
@@ -1,23 +1,5 @@
-import { Suspense } from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
+import { createRoot } from "react-dom/client";
+import App from "./App";
 import "./index.css";
-import { BrowserRouter } from "react-router-dom";
-import { StyleSheetManager } from "styled-components";
 
-// Ensure you have a <div id="root"></div> in your index.html
-const container = document.getElementById("root"); // Get the target container
-
-if (container) {
-	ReactDOM.createRoot(container).render( // Pass the container to createRoot
-		<Suspense fallback={<div>Loading...</div>}>
-			<BrowserRouter>
-				<StyleSheetManager shouldForwardProp={(prop) => prop !== "shake"}>
-					<App />
-				</StyleSheetManager>
-			</BrowserRouter>
-		</Suspense>
-	);
-} else {
-	console.error("Target container is not found. Make sure there is a <div id='root'></div> in your HTML.");
-}
+createRoot(document.getElementById("root")).render(<App />);
--- src/pages/Boards/index.jsx
+++ src/pages/Boards/index.jsx
@@ -8,7 +8,6 @@ import { useState } from "react";
 import { Board } from "../../data/board";
 import { onDragEnd } from "../../helpers/onDragEnd";
 import AddModal from "../../components/Modals/AddModal";
-import Task from "../../components/Task";
 
 function Home() {
   const [columns, setColumns] = useState(Board);
@@ -31,9 +30,10 @@ function Home() {
 
   return (
     <>
-      <DragDropContext onDragEnd={(result) => onDragEnd(result, columns, setColumns)}>
+      <DragDropContext
+        onDragEnd={(result) => onDragEnd(result, columns, setColumns)}
+      >
         <div className="w-full flex items-start justify-between px-5 bg-green-200">
-
           {Object.entries(columns).map(([columnId, column]) => (
             <div className="flex flex-col gap-2" key={columnId}>
               <Droppable droppableId={columnId} key={columnId}>
@@ -54,12 +54,70 @@ function Home() {
                       Add Task
                     </div>
                     {column.items.map((task, index) => (
-                      <Draggable key={task.id.toString()} draggableId={task.id.toString()} index={index}
+                      <Draggable
+                        key={task.id.toString()}
+                        draggableId={task.id.toString()}
+                        index={index}
                       >
                         {(provided) => (
+                          // Task START
                           <>
-                            <Task provided={provided} task={task} />
+                            <div
+                              ref={provided.innerRef}
+                              {...provided.draggableProps}
+                              {...provided.dragHandleProps}
+                              className="w-full cursor-grab bg-[#fff] flex flex-col justify-between gap-3 items-start shadow-sm rounded-xl px-3 py-4"
+                            >
+                              {task.image && task.alt && (
+                                <img
+                                  src={task.image}
+                                  alt={task.alt}
+                                  className="w-full h-[170px] rounded-lg"
+                                />
+                              )}
+                              <div className="flex items-center gap-2">
+                                {task.tags.map((tag) => (
+                                  <span
+                                    key={tag.title}
+                                    className="px-[10px] py-[2px] text-[13px] font-semibold rounded-md"
+                                    style={{
+                                      backgroundColor: tag.bg,
+                                      color: tag.text,
+                                    }}
+                                  >
+                                    {tag.title}
+                                  </span>
+                                ))}
+                              </div>
+                              <div className="w-full flex items-start flex-col gap-0">
+                                <span className="text-[15.5px] font-medium text-[#555]">
+                                  {task.title}
+                                </span>
+                                <span className="text-[13.5px] text-gray-500">
+                                  {task.description}
+                                </span>
+                              </div>
+                              <div className="w-full border border-dashed"></div>
+                              <div className="w-full flex items-center justify-between">
+                                <div className="flex items-center gap-1">
+                                  <i className="bi bi-clock"></i>
+                                  <span className="text-[13px] text-gray-700">
+                                    {task.deadline} mins
+                                  </span>
+                                </div>
+                                <div
+                                  className={`w-[60px] rounded-full h-[5px] ${
+                                    task.priority === "high"
+                                      ? "bg-red-500"
+                                      : task.priority === "medium"
+                                      ? "bg-orange-500"
+                                      : "bg-blue-500"
+                                  }`}
+                                ></div>
+                              </div>
+                            </div>
                           </>
+                          // Task END
                         )}
                       </Draggable>
                     ))}
@@ -69,7 +127,6 @@ function Home() {
               </Droppable>
             </div>
           ))}
-
         </div>
       </DragDropContext>
 
